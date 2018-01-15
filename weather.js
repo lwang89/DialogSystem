8ef008c0e95dc0690a3848549cb6ac42d5a5444f
Glitch (sparkling-toad) <none>
2018-01-15 14:10:05 +0000

    update weather API

---
+++ weather.js
@@ -0,0 +1,17 @@
+let request = require('request');
+
+let apiKey = '4767cfa6ea58a90ee0c3cce7f2cea070';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+    //console.log('body:', body);
+  }
+});
+
